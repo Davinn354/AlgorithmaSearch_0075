4b910e8948a6f1baa51c3fdc9853d3dc8547b5cd
Davinn354 <daviipad53@gmail.com>
2024-04-05 15:09:47 +0700

    penambahan integer i untuk membuat variable int i yang digunakan sebagai indeks literasi

--- PertemuanKetujuh/PertemuanKetujuh.cpp
+++ PertemuanKetujuh/PertemuanKetujuh.cpp
@@ -3,3 +3,4 @@ using namespace std;
 
 int arr[20]; //membuat variable global array dengan panjang maximum 20
 int n; //membuat variable global int n untuk menyimpan jumlah elemen yang diinginkan dalam array.
+int i; //membuat variable global int i yang digunakan sebagai indeks iterasi 
