25433544df9e5c1f65ca7750b44242be993f41d4
Davinn354 <daviipad53@gmail.com>
2024-04-05 15:15:13 +0700

    Menambahkan cin

--- PertemuanKetujuh/PertemuanKetujuh.cpp
+++ PertemuanKetujuh/PertemuanKetujuh.cpp
@@ -9,6 +9,7 @@ void input() // memulai prosedur input
 	while (true) //menambahkan looping while (true) didalam input ntuk membuat loop yang akan terus berjalan tanpa henti sampai ada instruksi break di dalamnya.
     {
         cout << "Enter the number of elements in the array : "; //menambahkan input output jumlah element yang diinginkan dalam array 
+        cin >> n;
         
 
     }
