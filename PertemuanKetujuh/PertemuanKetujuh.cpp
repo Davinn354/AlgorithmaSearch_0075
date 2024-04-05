7c494e6061709d3d33bfd3ed46f5ded8ee48772d
Davinn354 <daviipad53@gmail.com>
2024-04-05 15:11:33 +0700

    memasukkan kurung kurawal

--- PertemuanKetujuh/PertemuanKetujuh.cpp
+++ PertemuanKetujuh/PertemuanKetujuh.cpp
@@ -5,3 +5,6 @@ int arr[20]; //membuat variable global array dengan panjang maximum 20
 int n; //membuat variable global int n untuk menyimpan jumlah elemen yang diinginkan dalam array.
 int i; //membuat variable global int i yang digunakan sebagai indeks iterasi 
 void input() // memulai prosedur input
+{
+
+}
